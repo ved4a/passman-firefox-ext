766ac017e51df10635d4cc878687da5c9f50f0fa
Vedaa Anand <work.vedaa@gmail.com>
2025-04-21 13:51:46 +0530

    feat: add listeners to confirm ext run/install

--- background.js
+++ background.js
@@ -1,1 +1,10 @@
 "use strict";
+
+browser.runtime.onStartup.addListener(() => {
+    console.log("Extension started");
+  });
+  
+  browser.runtime.onInstalled.addListener(() => {
+    console.log("Installed extension");
+  });
+  
