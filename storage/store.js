642362f3716725efcac7453a87cad4d1d322f29d
Vedaa Anand <work.vedaa@gmail.com>
2025-04-21 13:36:45 +0530

    feat: retrieve from vault fun

--- storage/store.js
+++ storage/store.js
@@ -1,5 +1,14 @@
 "use strict";
 
+// use cases:
+// hashed master pw, salt, enc data, auto-lock timestamps
 export async function saveToStorage(key, value){
     await browser.storage.local.set({[key]: value});
+}
+
+// use case:
+// retrieve stored salt, check if master pw set, fetch enc vault
+export async function getFromStorage(key){
+    const result = await browser.storage.local.get(key);
+    return result[key];
 }
