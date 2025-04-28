4774cffb90cb2fb8a1f90690657154c577107e08
Vedaa Anand <work.vedaa@gmail.com>
2025-04-28 16:51:23 +0530

    fix: crypto function names, DOM load then init

--- pop-up/popup.js
+++ pop-up/popup.js
@@ -1,22 +1,29 @@
 "use strict";
 
-import { generateSalt, hashPassword } from '../crypto/crypto.js';
+import { genSalt, hashPW } from '../crypto/crypto.js';
 import { saveToStorage, getFromStorage } from '../storage/store.js';
 import { startAutoLock } from '../utils/timer.js';
 
-const setupDiv = document.getElementById('setup');
-const loginDiv = document.getElementById('login');
+// const setupDiv = document.getElementById('setup');
+// const loginDiv = document.getElementById('login');
 const vaultDiv = document.getElementById('vault');
 const passwordsListDiv = document.getElementById('passwordsList');
 const searchInput = document.getElementById('searchInput');
 
 const init = async () => {
+  console.log('Init function started!');
+
+  const setupDiv = document.getElementById('setup');
+  const loginDiv = document.getElementById('login');
+
   // check if salt is in storage -> means there exists a vault
   // ...associated with the user
   const salt = await getFromStorage("salt");
   if (!salt){
-    setupDiv.classList.add('visible');
+    setupDiv.classList.remove('hidden');
   } else {
-    loginDiv.classList.add('visible');
+    loginDiv.classList.remove('hidden');
   }
 };
+
+document.addEventListener('DOMContentLoaded', init);
