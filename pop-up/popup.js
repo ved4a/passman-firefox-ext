50cbb60b25d84feb10b29b4b54adfe5a27dceed4
Vedaa Anand <work.vedaa@gmail.com>
2025-05-03 23:30:54 +0530

    feat: if no match show error; logic for storage

--- pop-up/popup.js
+++ pop-up/popup.js
@@ -4,8 +4,6 @@ import { genSalt, hashPW } from '../crypto/crypto.js';
 import { saveToStorage, getFromStorage } from '../storage/store.js';
 import { startAutoLock } from '../utils/timer.js';
 
-// const setupDiv = document.getElementById('setup');
-// const loginDiv = document.getElementById('login');
 const vaultDiv = document.getElementById('vault');
 const passwordsListDiv = document.getElementById('passwordsList');
 const searchInput = document.getElementById('searchInput');
@@ -15,15 +13,54 @@ const init = async () => {
 
   const setupDiv = document.getElementById('setup');
   const loginDiv = document.getElementById('login');
+  const errorContainer = document.getElementById("errorContainer");
 
   // check if salt is in storage -> means there exists a vault
   // ...associated with the user
   const salt = await getFromStorage("salt");
   if (!salt){
+    // show initial set-up
     setupDiv.classList.remove('hidden');
+
+    // create salt
+    let salt = genSalt();
+
+    document.getElementById("setPasswordBtn").addEventListener("click", function() {
+      const newPassword = document.getElementById("newPassword").value;
+      const confirmPassword = document.getElementById("confirmPassword").value;
+
+      if (newPassword == confirmPassword){
+        // clear the error container (if error exists)
+        errorContainer.textContent = "";
+
+        // do NOT store actual pw! only store hashed pw
+        let hashedMasterPassword = hashPW(confirmPassword, salt);
+        saveToStorage(salt, hashedMasterPassword);
+
+        // now can show the vault!
+
+      } else {
+        // show an error message
+        errorContainer.textContent = "Error: Passwords don't match!";
+
+      }
+    } )
+
+
   } else {
     loginDiv.classList.remove('hidden');
   }
+
+  // if NO salt:
+  // let user create master pw, hash, and store
+  // generate salt for hashing
+
+
+  // if yes salt:
+  // let user log in with master pw
+  // compute the hashed pw with the one in storage
+  // yes? go to vault. no? 3 tries
+
 };
 
 document.addEventListener('DOMContentLoaded', init);
