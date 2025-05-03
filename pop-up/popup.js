043b076dce6bc0f66ea8a9dbbe69c5e6ff699a15
Vedaa Anand <work.vedaa@gmail.com>
2025-05-03 23:35:52 +0530

    feat: show vault when setup complete

--- pop-up/popup.js
+++ pop-up/popup.js
@@ -4,16 +4,15 @@ import { genSalt, hashPW } from '../crypto/crypto.js';
 import { saveToStorage, getFromStorage } from '../storage/store.js';
 import { startAutoLock } from '../utils/timer.js';
 
+const setupDiv = document.getElementById('setup');
+const loginDiv = document.getElementById('login');
+const errorContainer = document.getElementById("errorContainer");
 const vaultDiv = document.getElementById('vault');
 const passwordsListDiv = document.getElementById('passwordsList');
 const searchInput = document.getElementById('searchInput');
 
 const init = async () => {
   console.log('Init function started!');
-
-  const setupDiv = document.getElementById('setup');
-  const loginDiv = document.getElementById('login');
-  const errorContainer = document.getElementById("errorContainer");
 
   // check if salt is in storage -> means there exists a vault
   // ...associated with the user
@@ -38,6 +37,8 @@ const init = async () => {
         saveToStorage(salt, hashedMasterPassword);
 
         // now can show the vault!
+        setupDiv.classList.add("hidden");
+        vaultDiv.classList.remove("hidden");
 
       } else {
         // show an error message
